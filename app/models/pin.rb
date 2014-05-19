6ff2bb8f9ad7168a43ee4745ec948154e96dd672
laddhadhiraj <laddhadhiraj86@gmail.com>
2014-05-19 16:15:15 +0530

    Add name to users and validation to pin

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,7 @@ class Pin < ActiveRecord::Base
      has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => '/images/missing.png'
      validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
+     validates :image, presence: true
+     validates :description, presence: true
+
 end
