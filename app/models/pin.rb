af21a82a4ea329ee028548cd794f3e95b44a772f
laddhadhiraj <laddhadhiraj86@gmail.com>
2014-05-19 20:45:15 +0530

    reset file

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
      has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => '/images/missing.png'
-    # validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+     validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
      validates :image, presence: true
      validates :description, presence: true
