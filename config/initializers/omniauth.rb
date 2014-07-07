64882400a119cf8cc8101f1bcd123dddf60c51eb
laddhadhiraj <laddhadhiraj86@gmail.com>
2014-07-07 23:15:03 +0530

    made config change

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,3 +8,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
  
   # If you want to also configure for additional login services, they would be configured here.
 end
+
+
+# heroku config:add LINKEDIN_KEY='75yhsygwuq87vi' LINKEDIN_SECRET='3nlbqM9Gi3ZnKK4k' FACEBOOK_KEY='533011503477554' FACEBOOK_SECRET='240444b885ade0fc4e1f5b54b815a98d' TWITTER_KEY='Tp9pRlP0LYFSjzHb00C6bK2y4' TWITTER_SECRET='wGpKCkXu0wXyoghC6R21TkNH3ddY3XPA44LeBen0O5PgarYtYE'
